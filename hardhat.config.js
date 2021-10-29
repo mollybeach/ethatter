8a098c285d191597eba21158d1899734cdd058d0
mollybeach <mollyjbeach@gmail.com>
2021-10-29 12:04:35 -0700

    hard hat config

---
+++ hardhat.config.js
@@ -0,0 +1,28 @@
+
+//import { createRequire } from "module"; // add the ability to construct the'require' method in js file
+//const require = createRequire(import.meta.url); // construct the require method 
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-ethers");
+require("@nomiclabs/hardhat-etherscan");
+require("@nomiclabs/hardhat-truffle5");
+require("@nomiclabs/hardhat-solhint");
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: "0.8.4",
+};
